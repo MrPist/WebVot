795137fc64554db4efbfe2b0a89403c68b80f225
MrPist <phuocle161002@gmail.com>
2023-12-06 10:43:23 +0700

    insert_sp&change_layout_home(2)

--- WebVot/WebVot/Controllers/HomeController.cs
+++ WebVot/WebVot/Controllers/HomeController.cs
@@ -46,6 +46,129 @@ namespace WebVot.Controllers
             return View(sanPham);
         }
 
-        
+        // GET: Home/Create
+        public IActionResult Create()
+        {
+            ViewData["MaNcc"] = new SelectList(_context.Nccs, "MaNcc", "MaNcc");
+            ViewData["MaNsx"] = new SelectList(_context.Nsxes, "MaNsx", "MaNsx");
+            return View();
+        }
+
+        // POST: Home/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaSp,TenSp,Sluong,MaNsx,MaNcc,Hinhanh,GiaBan,GiaGoc")] SanPham sanPham)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(sanPham);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNcc"] = new SelectList(_context.Nccs, "MaNcc", "MaNcc", sanPham.MaNcc);
+            ViewData["MaNsx"] = new SelectList(_context.Nsxes, "MaNsx", "MaNsx", sanPham.MaNsx);
+            return View(sanPham);
+        }
+
+        // GET: Home/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null || _context.SanPhams == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+            ViewData["MaNcc"] = new SelectList(_context.Nccs, "MaNcc", "MaNcc", sanPham.MaNcc);
+            ViewData["MaNsx"] = new SelectList(_context.Nsxes, "MaNsx", "MaNsx", sanPham.MaNsx);
+            return View(sanPham);
+        }
+
+        // POST: Home/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("MaSp,TenSp,Sluong,MaNsx,MaNcc,Hinhanh,GiaBan,GiaGoc")] SanPham sanPham)
+        {
+            if (id != sanPham.MaSp)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(sanPham);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SanPhamExists(sanPham.MaSp))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["MaNcc"] = new SelectList(_context.Nccs, "MaNcc", "MaNcc", sanPham.MaNcc);
+            ViewData["MaNsx"] = new SelectList(_context.Nsxes, "MaNsx", "MaNsx", sanPham.MaNsx);
+            return View(sanPham);
+        }
+
+        // GET: Home/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null || _context.SanPhams == null)
+            {
+                return NotFound();
+            }
+
+            var sanPham = await _context.SanPhams
+                .Include(s => s.MaNccNavigation)
+                .Include(s => s.MaNsxNavigation)
+                .FirstOrDefaultAsync(m => m.MaSp == id);
+            if (sanPham == null)
+            {
+                return NotFound();
+            }
+
+            return View(sanPham);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            if (_context.SanPhams == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.SanPhams'  is null.");
+            }
+            var sanPham = await _context.SanPhams.FindAsync(id);
+            if (sanPham != null)
+            {
+                _context.SanPhams.Remove(sanPham);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SanPhamExists(string id)
+        {
+          return (_context.SanPhams?.Any(e => e.MaSp == id)).GetValueOrDefault();
+        }
     }
 }
